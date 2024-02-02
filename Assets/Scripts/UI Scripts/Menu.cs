e06289e9bda766350267d876156f8f8029f0d5ae
Cian Farrell <cianfarrell02@gmail.com>
2024-02-02 19:37:39 +0000

    Added tagline

--- Assets/Scripts/UI Scripts/Menu.cs
+++ Assets/Scripts/UI Scripts/Menu.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,11 +9,17 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public Button startButton;
+    public TextMeshProUGUI StartText;
 
     // Start is called before the first frame update
     void Start()
     {
-        startButton.onClick.AddListener(StartGame);
+        //Show the cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        
+        startButton.onClick.AddListener(() => StartCoroutine(DisplaySplashScreen()));
+        StartText.gameObject.SetActive(false);
     }
 
     void StartGame()
@@ -21,5 +31,24 @@ public class Menu : MonoBehaviour
         int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
         
         SceneManager.LoadScene(nextSceneIndex);
+    }
+    
+    IEnumerator DisplaySplashScreen()
+    {
+        //Find all items on ui layer
+        var allGameObjects = FindObjectsOfType<GameObject>();
+        var uiItems = allGameObjects.Where(go => go.layer == 5).ToList();
+        
+        foreach (var uiItem in uiItems)
+        {
+            if (uiItem.name == "Canvas")
+                continue;
+            
+            uiItem.SetActive(false);
+        }
+        
+        StartText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        StartGame();
     }
 }
